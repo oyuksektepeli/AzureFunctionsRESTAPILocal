d92ace7f17081fc410a7a40c793ae2f943d4617f
Onur Yüksektepeli <onuryuksektepeli@hotmail.com.tr>
2020-03-19 02:07:23 +0300

    Add project files.

---
+++ AzureFunctionsRESTAPILocal/Function1.cs
@@ -0,0 +1,91 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Microsoft.Azure.WebJobs.Host;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AzureFunctionsRESTAPILocal
+{
+    public static class ToDoAAPI
+    {
+        static List<Todo> items = new List<Todo>();
+
+        [FunctionName("CreateTodo")]
+        public static async Task<IActionResult> CreateTodo(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "todo")]HttpRequest req, ILogger log)
+        {
+            log.LogInformation("Creating a new todo list item");
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            var input = JsonConvert.DeserializeObject<TodoCreateModel>(requestBody);
+
+            var todo = new Todo() { TaskDescription = input.TaskDescription };
+            items.Add(todo);
+            return new OkObjectResult(todo);
+        }
+
+        [FunctionName("GetTodos")]
+        public static IActionResult GetTodos(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "todo")]HttpRequest req, ILogger log)
+        {
+            log.LogInformation("Getting todo list items");
+            return new OkObjectResult(items);
+        }
+
+        [FunctionName("GetTodoById")]
+        public static IActionResult GetTodoById(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "todo/{id}")]HttpRequest req,
+            ILogger log, string id)
+        {
+            var todo = items.FirstOrDefault(t => t.Id == id);
+            if (todo == null)
+            {
+                return new NotFoundResult();
+            }
+            return new OkObjectResult(todo);
+        }
+
+        [FunctionName("UpdateTodo")]
+        public static async Task<IActionResult> UpdateTodo(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "put", Route = "todo/{id}")]HttpRequest req,
+            ILogger log, string id)
+        {
+            var todo = items.FirstOrDefault(t => t.Id == id);
+            if (todo == null)
+            {
+                return new NotFoundResult();
+            }
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            var updated = JsonConvert.DeserializeObject<TodoUpdateModel>(requestBody);
+
+            todo.IsCompleted = updated.IsCompleted;
+            if (!string.IsNullOrEmpty(updated.TaskDescription))
+            {
+                todo.TaskDescription = updated.TaskDescription;
+            }
+
+            return new OkObjectResult(todo);
+        }
+
+        [FunctionName("DeleteTodo")]
+        public static IActionResult DeleteTodo(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "delete", Route = "todo/{id}")]HttpRequest req,
+            ILogger log, string id)
+        {
+            var todo = items.FirstOrDefault(t => t.Id == id);
+            if (todo == null)
+            {
+                return new NotFoundResult();
+            }
+            items.Remove(todo);
+            return new OkResult();
+        }
+    }
+}
---
+++ AzureFunctionsRESTAPILocal/Model.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AzureFunctionsRESTAPILocal
+{
+    public class Todo
+    {
+        public string Id { get; set; } = Guid.NewGuid().ToString("n");
+        public DateTime CreatedTime { get; set; } = DateTime.UtcNow;
+        public string TaskDescription { get; set; }
+        public bool IsCompleted { get; set; }
+    }
+
+    public class TodoCreateModel
+    {
+        public string TaskDescription { get; set; }
+    }
+
+    public class TodoUpdateModel
+    {
+        public string TaskDescription { get; set; }
+        public bool IsCompleted { get; set; }
+    }
+}
